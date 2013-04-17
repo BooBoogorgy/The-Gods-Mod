013837e3f01ddbd4e5662f88ea6deb3e8ea8b0f6
unlishema <unlishema.spam@gmail.com>
2013-04-17 10:55:44 -0300

    Update GodsMod.java

--- common/com/gods/mod/GodsMod.java
+++ common/com/gods/mod/GodsMod.java
@@ -1,5 +1,7 @@
 package com.gods.mod;
 
+import java.util.ArrayList;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
@@ -13,80 +15,82 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-
 @Mod(modid = GodsMod.modid, name = "Gods Mod", version = "0.0.1 Alpha")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
-public class GodsMod 
-{
-	
+public class GodsMod {
+
 	@Instance("Manitor")
 	public static GodsMod Instatnce;
-	
+
 	static int startEntityId = 350;
-	
-    public static final String modid = "Gods_GodsMod";
-    
-    public static CreativeTabs GodsTab = new GodTab("The Gods Creative Tab");
-    
-    
-    
-    public static Block WhiteGrass = new WhiteGrass(170, 0).setUnlocalizedName("WhiteGrass").setHardness(0.5F).setStepSound(Block.soundGrassFootstep);
-	public static Block WhiteDirt = new GodDirt(171, 0, "WhiteGrass_Bottom").setUnlocalizedName("WhiteDirt").setHardness(0.4F).setStepSound(Block.soundGrassFootstep);
-    public static Block CrackedMossyStone = new CrackedMossy(1002, Material.rock).setUnlocalizedName("CrackedMossyStone").setHardness(3.0F).setStepSound(Block.soundStoneFootstep);
-    public static Block ZeusBlock = new ZeusBlock(1003, Material.rock).setUnlocalizedName("ZeusBlock").setHardness(10.0F);
-    public static Block StonePiller = new StonePiller(1004, Material.rock).setUnlocalizedName("StonePiller").setHardness(6.0F);
-    public static Block AtlanticGlass = new AtlanticGlass(1005, Material.glass).setUnlocalizedName("AtlanticGlass").setHardness(1.0F).setLightOpacity(1);
-    public static Block GodsLeaves = new GodsLeaves(1006, 0, "GodsMod:GodsLeaves").setUnlocalizedName("GodsLeaves").setHardness(0.2F);
-    public static Block GodsWood = new GodsWood(1007, 0, Material.wood).setUnlocalizedName("GodsWood").setHardness(2.0F);
-    public static Block GodsSapling = new GodsSapling(1008, 1).setUnlocalizedName("GodsSapling").setHardness(0.0F);
-    
-    
-    
-    public static Item Staff = new GodsItem(1004, "GodsMod:Staff").setUnlocalizedName("Staff").setCreativeTab(GodsTab);
-	public static Item Trident = new GodsItem(1005, "GodsMod:Trident").setUnlocalizedName("Trident").setCreativeTab(GodsTab);
-	public static Item LightningBolt = new Lightning(1006, "GodsMod:LightningBolt").setUnlocalizedName("LightningBolt").setCreativeTab(GodsTab);
-	
-	
-	public BiomeGenBase WhiteBiome = new WhiteBiome(44);
-	
-	
-	
 
-    
-    @SuppressWarnings("deprecation")
-    @Init
-    public void load(FMLInitializationEvent event) 
-    {
+	public static final String modid = "Gods_GodsMod";
 
-    	
-		//Game Registry
-		GameRegistry.registerBlock(CrackedMossyStone);	
-		GameRegistry.registerBlock(WhiteGrass);	
-		GameRegistry.registerBlock(ZeusBlock);	
-		GameRegistry.registerBlock(StonePiller);	
-		GameRegistry.registerBlock(WhiteDirt);	
-		GameRegistry.registerBlock(AtlanticGlass);
-		GameRegistry.registerBlock(GodsLeaves);
-		GameRegistry.registerBlock(GodsSapling);
-		GameRegistry.registerBlock(GodsWood);
-		GameRegistry.addBiome(WhiteBiome);
+	public static final CreativeTabs GodsTab = new GodTab("The Gods Creative Tab");
+	
+	private static final String[] blockNames = new String[] {"White Grass", "White Dirt", "Cracked Mossy Stone", "Zeus Block", "Stone Piller", "Atlantic Glass", "Gods Leaves", "Gods Wood", "Gods Sapling"};
+	private static final String[] itemNames = new String[] {"Staff", "Trident", "Lightning Bolt"};
+
+	private static final ArrayList<Block> blocks = new ArrayList<Block>();
+	private static final ArrayList<Item> items = new ArrayList<Item>();
+
+	public static final Block WhiteGrass = new WhiteGrass(170, 0).setHardness(0.5F).setStepSound(Block.soundGrassFootstep);
+	public static final Block WhiteDirt = new GodDirt(171, 0, "WhiteGrass_Bottom").setHardness(0.4F).setStepSound(Block.soundGrassFootstep);
+	public static final Block CrackedMossyStone = new CrackedMossy(1002,Material.rock).setHardness(3.0F).setStepSound(Block.soundStoneFootstep);
+	public static final Block ZeusBlock = new ZeusBlock(1003, Material.rock).setHardness(10.0F);
+	public static final Block StonePiller = new StonePiller(1004, Material.rock).setHardness(6.0F);
+	public static final Block AtlanticGlass = new AtlanticGlass(1005, Material.glass).setHardness(1.0F).setLightOpacity(1);
+	public static final Block GodsLeaves = new GodsLeaves(1006, 0,"GodsMod:GodsLeaves").setHardness(0.2F);
+	public static final Block GodsWood = new GodsWood(1007, 0, Material.wood).setHardness(2.0F);
+	public static final Block GodsSapling = new GodsSapling(1008, 1).setHardness(0.0F);
+	static {
+		blocks.add(WhiteGrass);
+		blocks.add(WhiteDirt);
+		blocks.add(CrackedMossyStone);
+		blocks.add(ZeusBlock);
+		blocks.add(StonePiller);
+		blocks.add(AtlanticGlass);
+		blocks.add(GodsLeaves);
+		blocks.add(GodsWood);
+		blocks.add(GodsSapling);
+	}
+
+	public static final Item Staff = new GodsItem(1004, "GodsMod:Staff").setCreativeTab(GodsTab);
+	public static final Item Trident = new GodsItem(1005, "GodsMod:Trident").setCreativeTab(GodsTab);
+	public static final Item LightningBolt = new Lightning(1006,"GodsMod:LightningBolt").setCreativeTab(GodsTab);
+	static {
+		items.add(Staff);
+		items.add(Trident);
+		items.add(LightningBolt);
+	}
+
+	public final BiomeGenBase WhiteBiome = new WhiteBiome(44);
+
+	@Init
+	public void load(final FMLInitializationEvent event) {
 		
-	
-        
-        // Language registry
-		LanguageRegistry.addName(CrackedMossyStone, "Cracked Mossy Stone");
-		LanguageRegistry.addName(Staff, "Staff");
-		LanguageRegistry.addName(StonePiller, "Stone Piller");
-		LanguageRegistry.addName(AtlanticGlass, "Atlantic Glass");
-		LanguageRegistry.addName(Trident, "Trident");
-		LanguageRegistry.addName(ZeusBlock, "Zeus Block");
-		LanguageRegistry.addName(GodsWood, "Gods Leaves");
-		LanguageRegistry.addName(GodsSapling, "Gods Sapling");
-		LanguageRegistry.addName(GodsWood, "Gods Wood");
-		LanguageRegistry.addName(WhiteGrass, "White Grass");
-		LanguageRegistry.addName(WhiteDirt, "White Dirt");
-		LanguageRegistry.addName(LightningBolt, "Lightning Bolt");
-		LanguageRegistry.instance().addStringLocalization("itemGroup.The Gods Creative Tab", "en_US", "The Gods Mod");
-		LanguageRegistry.instance().addStringLocalization("itemGroup.The Gods Creative Tab", "nl_NL", "De Goden Mod");
-    }
+		// Set Block Names
+		for(int i = 0; i < blockNames.length; i++) {
+			blocks.get(i).setUnlocalizedName(blockNames[i].replaceAll(" ", ""));
+		}
+
+		// Block Registry
+		for(final Block block : blocks) {
+			GameRegistry.registerBlock(block, modid + "_" + block.getUnlocalizedName2());
+		}
+		GameRegistry.addBiome(this.WhiteBiome);
+
+		// Language registry
+		for(int i = 0; i < blockNames.length; i++) {
+			LanguageRegistry.addName(blocks.get(i), blockNames[i]);
+		}
+		for(int i = 0; i < itemNames.length; i++) {
+			LanguageRegistry.addName(items.get(i), itemNames[i]);
+		}
+		
+		LanguageRegistry.instance().addStringLocalization(
+				"itemGroup.The Gods Creative Tab", "en_US", "The Gods Mod");
+		LanguageRegistry.instance().addStringLocalization(
+				"itemGroup.The Gods Creative Tab", "nl_NL", "De Goden Mod");
+	}
 }
